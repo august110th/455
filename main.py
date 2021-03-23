ea8acf9bda5357586753260064333d333ad5edf3
august110th <august110th@ya.ru>
2021-03-23 22:51:59 +0400

    Initial commit

--- main.py
+++ main.py
@@ -162,12 +162,19 @@ worst_student.__str__()
 # print(Student.names)
 print(Student.grades)
 print(Lecturer.grades)
-# for i in Student.grades:
-#     if 'Python' in i:
-#         print(i)
+
 print(middle_student.grades)
 # print(Reviewer.names)
 # print(Lecturer_grades)
 # print(student_grades)
+resultdict = {}
+for dictionary in Student.grades:                                     # пробегаем по списку словарей
+    for key in dictionary:                               # пробегаем по ключам словаря
+        try:
+            resultdict[key] += dictionary[key]        # складываем значения
+        except KeyError:                                    # если ключа еще нет - создаем
+            resultdict[key] = dictionary[key]
+            if 'Python' in resultdict.keys():
+                print(resultdict.values())
 
 
