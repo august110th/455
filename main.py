f3e9e9b995ac45c0fee2f504a2dd955e2d3cc67d
august110th <80058477+august110th@users.noreply.github.com>
2021-03-24 10:31:05 +0400

    4545

--- main.py
+++ main.py
@@ -161,20 +161,16 @@ worst_student.__str__()
 # print(middle_student < worst_student)
 # print(Student.names)
 print(Student.grades)
-print(Lecturer.grades)
-
-print(middle_student.grades)
+# print(Lecturer.grades)
+#
+# print(middle_student.grades)
 # print(Reviewer.names)
 # print(Lecturer_grades)
 # print(student_grades)
 resultdict = {}
-for dictionary in Student.grades:                                     # пробегаем по списку словарей
-    for key in dictionary:                               # пробегаем по ключам словаря
-        try:
-            resultdict[key] += dictionary[key]        # складываем значения
-        except KeyError:                                    # если ключа еще нет - создаем
-            resultdict[key] = dictionary[key]
-            if 'Python' in resultdict.keys():
-                print(resultdict.values())
+for dictionary in Student.grades:
+    for key in dictionary:
+        if 'Python' in dictionary:
+            print(sum(dictionary[key]))
 
 
