c117e32d74a183f6df3cfd2b41ee0b91b03bdc43
august110th <august110th@ya.ru>
2021-03-30 21:31:51 +0400

    Initial commit

--- 456.py
+++ 456.py
@@ -1,65 +1,19 @@
 cook_book = {}
-#   'Омлет': [
-#     {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-#     {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-#     {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
+#     cook_book = {
+#     'Омлет': [
+#         {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
+#         {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
+#         {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
 #     ],
-#   'Утка по-пекински': [
-#     {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-#     {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-#     {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-#     {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
+#     'Утка по-пекински': [
+#         {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
+#         {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
+#         {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
+#         {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
 #     ],
-#   'Запеченный картофель': [
-#     {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-#     {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-#     {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
+#     'Запеченный картофель': [
+#         {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
+#         {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
+#         {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
 #     ]
-#   }
-
-def cook_book():
-    list_dish_name = []
-    list_ingridient = []
-    with open("recipes.txt", encoding='utf8') as file:
-        for ingredient in file:
-            key = ingredient.strip().lower()
-            cook_book[key] = []
-            for _ in range(int(file.readline())):
-                ingredient_name, quantity, measure = file.readline().strip().split(' | ')
-                cook_book[key].append(
-                    {'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure})
-            file.readline()
-    return cook_book
-
-print(cook_book)
-
-
-def get_shop_list_by_dishes(dishes, person_count):
-    shop_list = {}
-    for dish in dishes:
-        for ingridient in cook_book[dish]:
-            new_shop_list_item = dict(ingridient)
-
-            new_shop_list_item['quantity'] *= person_count
-            if new_shop_list_item['ingridient_name'] not in shop_list:
-                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
-            else:
-                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
-
-
-    return shop_list
-
-
-def print_shop_list(shop_list):
-    for shop_list_item in shop_list.values():
-        print(
-            '{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
-
-
-def create_shop_list():
-    person_count = int(input('Введите количество человек: '))
-    dishes = input('Введите блюда в расчете на одного человека (через запятую): ').lower().split(', ')
-    shop_list = get_shop_list_by_dishes(dishes, person_count)
-    print_shop_list(shop_list)
-
-create_shop_list()
+# }
