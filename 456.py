2db48337996fb2ffb8bc1900bfaac1533737e47a
august110th <august110th@ya.ru>
2021-04-04 23:01:27 +0400

    Initial commit

--- 456.py
+++ 456.py
@@ -42,6 +42,8 @@ def get_shop_list_by_dishes(dishes, person_count):
     shop_list = {}
     for dish in dishes:
         for ingredient in cook_book[dish]:
+            # print(cook_book[dish])
+            print(ingredient.items())
             new_shop_list_item = dict(ingredient)
             new_shop_list_item['quantity'] *= person_count
             if new_shop_list_item['ingredient_name'] not in shop_list:
@@ -50,14 +52,12 @@ def get_shop_list_by_dishes(dishes, person_count):
                 shop_list[new_shop_list_item['ingredient_name']]['quantity'] += new_shop_list_item['quantity']
     return shop_list
 
-def print_shop_list(shop_list):
-    for shop_list_item in shop_list.values():
-        print('{} {} {}'.format(shop_list_item['ingredient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+
 
 def create_shop_list():
     person_count = int(input('Введите нужное количество человек: '))
     dishes = input('Введите требуемые блюда в расчете на одного человека (через запятую): ').lower().split(', ')
     shop_list = get_shop_list_by_dishes(dishes, person_count)
-    print_shop_list(shop_list)
+    print(shop_list)
 
 create_shop_list()
