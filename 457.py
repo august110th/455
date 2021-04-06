d52942a5b0912eea500eb7987262ea194ca5ff80
august110th <august110th@ya.ru>
2021-04-06 19:55:59 +0400

    Initial commit

--- 457.py
+++ 457.py
@@ -1,3 +1,9 @@
 with open('1.txt', encoding='utf-8') as f:
-    print(f.readline())
+    list1 = (f.readlines())
 
+with open('2.txt', encoding='utf-8') as f:
+    list2 = (f.readlines())
+
+with open('3.txt', encoding='utf-8') as f:
+    list3 = (f.readlines())
+print(list2)
