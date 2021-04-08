cbca57b38c37dc14c94ae8f0942ff4fabcfbbe04
august110th <august110th@ya.ru>
2021-04-08 17:59:02 +0400

    Initial commit

--- 457.py
+++ 457.py
@@ -9,24 +9,12 @@ class Text:
             self.doc_list = (f.readlines())
             self.length = len(self.doc_list)
 
-    def write_file(self):
-        with open("4.txt", "w") as f:
-            f.write(self.name + '\n')
-            f.write(str(self.length) + '\n')
-            for item in self.doc_list:
-                f.write(item)
-
-    def search(self):
-        a = min(fragment1.length, fragment2.length, fragment3.length)
-        b = max(fragment1.length, fragment2.length, fragment3.length)
-        list_file = [fragment1.length, fragment2.length, fragment3.length]
-        for self.length in list_file:
-            if self.length == a:
-                self.write_file()
-            if a < self.length < b:
-                self.write_file()
-            if self.length == b:
-                self.write_file()
+def write_file(Text):
+    with open("4.txt", "a") as f:
+        f.write(Text.name + '\n')
+        f.write(str(Text.length) + '\n')
+        for item in Text.doc_list:
+            f.write(item)
 
 fragment1 = Text("1.txt")
 fragment1.doc_list_create()
@@ -36,3 +24,14 @@ fragment2.doc_list_create()
 
 fragment3 = Text("3.txt")
 fragment3.doc_list_create()
+
+a = min(fragment1.length, fragment2.length, fragment3.length)
+b = max(fragment1.length, fragment2.length, fragment3.length)
+dict_file = {fragment1 : fragment1.length, fragment2 : fragment2.length, fragment3 : fragment3.length}
+for key, value in dict_file.items():
+    if value == a:
+        write_file(key)
+    elif a < value < b:
+        write_file(key)
+    elif value == b:
+        write_file(key)
