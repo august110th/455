a54039013bec9ccaa4f28457f601195d90199e5c
august110th <august110th@ya.ru>
2021-04-12 23:04:04 +0400

    Initial commit

--- 458.py
+++ 458.py
@@ -1,34 +1,13 @@
 import requests
 import json
 
-# def __init__(self, token):
-#   self.token = token
-#
-# def get_headers(self):
-#   return {
-#
-#
-#   }
-#
-#
-#
-#
-#
-#
-#
-#
-
-
-Token = 2619421814940190
+# Token = 2619421814940190
 
 
 def request():
     url = "https://superheroapi.com/api/2619421814940190/search/"
-    params = {"name": "Hulk"}
-    headers = {"Authorization": "Token"}
-    return requests.get(url, params=params, headers=headers, timeout=5)
+    params = {}
+    return = requests.get(url, params=params, timeout=5)
 
-if __name__ == "__main__":
-  print(request().json())
 
 
