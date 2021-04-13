dd3d858914586d95b8ce04a81280b0971f6ae04b
august110th <august110th@ya.ru>
2021-04-13 20:17:32 +0400

    Initial commit

--- 458.py
+++ 458.py
@@ -1,20 +1,29 @@
 import requests
-import json
 
-# Token = 2619421814940190
+class Hero():
+
+    def __init__(self, intelligence, id, name):
+        self.intelligence = intelligence
+        self.id = id
+        self.name = name
+
+    def hero_stat(self):
+        hero_dict = requests.get(("https://superheroapi.com/api/2619421814940190/search/Hulk") + self.name).json()
+        self.intelligence = hero_dict['results'][0]['powerstats']["intelligence"]
+        self.id = hero_dict['results'][0]['id']
+        print(hero_dict)
 
 
-def request():
-    url = "https://superheroapi.com/api/2619421814940190/search/Hulk"
-    params = {}
-    return requests.get(url, params=params, timeout=5)
-print(request().json())
 
-# def hero_intelligence():
-hero_dict = requests.get("https://superheroapi.com/api/2619421814940190/search/Hulk").json()
-hero_int = hero_dict['results'][0]['powerstats']["intelligence"]
-print(hero_dict)
+# def request():
+#     url = "https://superheroapi.com/api/2619421814940190/search/Hulk"
+#     params = {}
+#     return requests.get(url, params=params, timeout=5)
+# print(request().json())
 
-#     return
-# hero_intelligence()
-print(hero_int)
+
+
+# #     return
+# # hero_intelligence()
+# print(hero_int)
+# print(hero_id)
