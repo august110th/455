5f016ee843caa9bfde72fc447026a507a295fcd4
august110th <august110th@ya.ru>
2021-04-13 13:52:50 +0400

    Initial commit

--- 458.py
+++ 458.py
@@ -7,7 +7,8 @@ import json
 def request():
     url = "https://superheroapi.com/api/2619421814940190/search/"
     params = {}
-    return = requests.get(url, params=params, timeout=5)
+    headers = {'Authorization': '2619421814940190'}
+    return requests.get(url, params=params, headers=headers, timeout=5)
 
 
 
