ae0677a177af826dc91435ffc5ed760cf18ad20b
august110th <august110th@ya.ru>
2021-04-14 16:15:00 +0400

    Initial commit

--- 458.py
+++ 458.py
@@ -11,36 +11,21 @@ class Hero():
         hero_dict = requests.get(("https://superheroapi.com/api/2619421814940190/search/") + self.name).json()
         self.intelligence = hero_dict['results'][0]['powerstats']["intelligence"]
         self.id = hero_dict['results'][0]['id']
-        # stat_dict = {}
-        # stat_dict[self.name] += [self.intelligence]
-        print(hero_dict)
-        # print(stat_dict)
+
+    def stat_dict(self):
+        stat_dict = {}
+        stat_dict[self.name] = self.intelligence
+        return stat_dict      # print(hero_dict)
+
 hulk = Hero("Hulk")
 captain_america = Hero("Captain America")
 thanos = Hero("Thanos")
 
 hulk.hero_stat()
-# captain_america.hero_stat()
-# thanos.hero_stat()
+captain_america.hero_stat()
+thanos.hero_stat()
+hulk.stat_dict()
 
 # print(stat_dict)
-# def stat_dict_get(Hero):
-#     stat_dict = {}
-#
 
 
-
-
-
-# def request():
-#     url = "https://superheroapi.com/api/2619421814940190/search/Hulk"
-#     params = {}
-#     return requests.get(url, params=params, timeout=5)
-# print(request().json())
-
-
-
-# #     return
-# # hero_intelligence()
-# print(hero_int)
-# print(hero_id)
