b7b3ae037fbb32b9ad8065c8891a55ce44e78645
august110th <august110th@ya.ru>
2021-04-14 23:05:27 +0400

    Initial commit

--- 459.py
+++ 459.py
@@ -3,6 +3,10 @@ class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
+    def get_headers(self):
+        return {"Content-Type" : "application/json()", "Authorization" : "OAuth {}".format(self.token)}
+
+
     def _get_upload_link(self, disk_file_path):
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = {"Authorization": f"OAuth {self.token}"}
@@ -13,13 +17,14 @@ class YaUploader:
 
     def upload_file_to_disk(self, disk_file_path, filename):
         href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
-        response = requests.put(href, data=open("to_upload.txt", "rb"))
+        response = requests.put(href, data=open(filename, "rb"))
         response.raise_for_status()
         if response.status_code == 201:
             print("Succes")
 
 #
 if __name__ == '__main__':
+    Token = "AQAAAAABShuLAADLW4R_AUOH0EkEqr8hgVITfn0"
     uploader = YaUploader('2303acdcd34a428db8778a708d56a056')
-    result = uploader.upload_file_to_disk("upload/to_upload.txt", "to_upload.txt")
+    print(uploader.upload_file_to_disk("upload/to_upload.txt", "c:/temp/to_upload.txt"))
 # print(result)
