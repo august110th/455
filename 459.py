79a3990520ed78810f75826c4ed7a0e1e8e1ba05
august110th <august110th@ya.ru>
2021-04-15 09:16:33 +0400

    Initial commit

--- 459.py
+++ 459.py
@@ -6,3 +6,8 @@ class YaUploader:
         self.token = token
 
     def upload(self, file_path: str):
+        url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        params = {"path": file_path, "overwrite": "True"}
+        headers = {"Authorization": f"OAuth {self.token}"}
+        response = requests.get(url, params=params, headers=headers)
+        result = response.json()["href"]
