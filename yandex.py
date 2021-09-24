e6d1b84e06149ae0f7e5b8564520ef4473553525
august110th <80058477+august110th@users.noreply.github.com>
2021-09-24 16:24:47 +0400

    done

--- yandex.py
+++ yandex.py
@@ -4,7 +4,7 @@ class NewFolder:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, path: str):
+    def folder_create(self, path: str):
         url = "https://cloud-api.yandex.net/v1/disk/resources"
         params = {"path": "new_folder"}
         headers = {"Authorization": f"OAuth {self.token}"}
@@ -13,4 +13,4 @@ class NewFolder:
 
 if __name__ == "__main__":
     nf = NewFolder("AQAAAAABShuLAADLW29yEpv3dkkDj0C1Fd1qU9M")
-    print(nf.upload("new_folder"))
+    print(nf.folder_create("new_folder"))
